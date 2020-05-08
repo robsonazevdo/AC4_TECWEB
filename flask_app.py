7bc4f0dd873d72c9c2cb3efa43ec7a9fb0ce0536
robsonazevdo <kassapete@hotmail.com>
2020-05-08 10:53:29 -0300

    ac04

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/inicio')
+@app.route("/inicio")
 @app.route('/')
 def inicio():
     return  render_template('inicio.html')
